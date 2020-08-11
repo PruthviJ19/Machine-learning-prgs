74bac84218d28ab7713e13c558b759d0ef0c6d7a
PruthviJ19 <68417132+PruthviJ19@users.noreply.github.com>
2020-08-11 20:58:58 +0530

    Add files via upload

---
+++ distribution pgr in ML.py
@@ -0,0 +1,27 @@
+import numpy
+import matplotlib.pyplot as plt
+x=numpy.random.uniform(0.0,5.0,250)             #random distribution
+#plt.hist(x,5)
+#plt.show()
+
+
+
+
+import numpy                                   #normal dist(probability dist)
+import matplotlib.pyplot as plt
+x=numpy.random.normal(5.0,1.0,100000)
+#plt.hist(x,100)
+#plt.show()
+
+import matplotlib.pyplot as plt
+x=[5,7,8,7,2,17,2,9,4,11,12,9,6]                  #scatter()method
+y=[99,86,87,88,111,86,103,87,94,78,77,85,86]
+#plt.scatter(x,y)
+#plt.show()
+
+import numpy
+import matplotlib.pyplot as pltx
+x=numpy.random.normal(5.0,1.0,1000)           #random data distributions
+y=numpy.random.normal(10.0,2.0,1000)
+#plt.scatter(x,y)
+#plt.show()
